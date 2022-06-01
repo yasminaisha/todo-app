153a0905228cb9ba357e1dc18d152debaf6cdd11
yasminaisha <jasyahmed1@yahoo.com>
2022-06-01 19:09:44 +0300

    styling & js updates

---
+++ script.js
@@ -0,0 +1,87 @@
+var list = document.getElementById("todos-list");
+var addBtn = document.getElementById("todo-add-btn");
+var addInput = document.getElementById("todo-input");
+
+function createTodo() {
+
+    var text = addInput.value;
+
+    if (text === "") {
+        return;
+    }
+
+    var li = document.createElement("li");
+
+    var checkbox = document.createElement("input");
+    checkbox.classList.add("checkbox");
+    checkbox.type = "checkbox";
+
+    var paragraph = document.createElement("p");
+    paragraph.classList.add("paragraph");
+    paragraph.textContent = text;
+
+    var remove = document.createElement('span');
+    remove.classList.add("remove");
+    remove.innerHTML = "&cross;";
+
+    li.appendChild(checkbox);
+    li.appendChild(paragraph);
+    li.appendChild(remove);
+    list.appendChild(li);
+
+    addInput.value = "";
+}
+
+function showEditInput(paragraph) {
+
+    var editInput = document.getElementsByName("editInput")[0];
+    if (editInput) {
+        editInput.remove();
+    }
+
+    var input = document.createElement("input");
+    input.type = "text";
+    input.name = "editInput";
+    input.value = paragraphElement.textContent;
+    input.classList.add("editInput");
+
+    paragraphElement.parentElement.appendChild(input);
+    input.focus();
+}
+
+function removeTodo(removeElement) {
+    removeElement.parentElement.remove();
+}
+
+function toggleComplete(inputElement) {
+    if (inputElement.checked === false) {
+        inputElement.parentElement.classList.remove("complete");
+    } else {
+        inputElement.parentElement.classList.remove("complete");
+    }
+}
+list.addEventListener("click", function (event){
+
+    switch (event.target.tagName) {
+        case "p":
+            showEditInput();
+            break;
+        case "SPAN":
+            removeTodo(event.target);
+            break;    
+    }
+});
+
+list.addEventListener("change", function (event) {
+    if (event.target.tagName === "INPUT" && event.target.type === "checkbox") {
+        toggleComplete();
+    }
+});
+
+addBtn.addEventListener("click", createTodo);
+
+addInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        createTodo();
+    }
+});
